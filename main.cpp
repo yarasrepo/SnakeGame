0a2f41db46d986cdc8618b322a77804c7a0672c2
Yara Al Tassi <145348472+yarasrepo@users.noreply.github.com>
2023-12-24 15:55:15 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
-#include <cstdlib>
 #include "Snake.h"
+#include "Food.h"
 #include <conio.h>
+#include <ctime>
 //https://www.youtube.com/watch?v=AxrQje7V65o&ab_channel=ProgrammingKnowledge 
-//30
+//1:00:00
 #define WIDTH 50
 #define HEIGHT 25
 using namespace std;
 
 Snake snake({WIDTH/2, HEIGHT/2}, 1);
+Food food;
 
 void board(){
 
     COORD snakePos= snake.getPos();
+    COORD foodPos= food.getPos();
 
     for (int i=0; i< HEIGHT; i++){
         cout << "#";
         for (int j=0; j<WIDTH-2; j++){
             if (i==0 || i== HEIGHT-1)
                 cout << '#';
-            else if (i==snakePos.Y && j==snakePos.X)
+            else if (i==snakePos.Y && j+1==snakePos.X)
                 cout << '0';
+            else if (i == foodPos.Y && j +1== foodPos.X)
+                cout << '*';
             else 
                 cout << ' ';
         }
@@ -29,7 +34,11 @@ void board(){
 }
 
 int main(){
-    while (true){
+    srand(time(NULL));
+    food.genFood();
+    bool gameOver = false;
+
+    while (!gameOver){
         board();
 
         if (kbhit()){
@@ -47,8 +56,18 @@ int main(){
                     snake.changeDir('r');
                     break;
             }
-            snake.moveSnake();
         }
+        if (snake.collided()){
+                gameOver= true;
+            }
+
+            if (snake.eaten(food.getPos())){
+                food.genFood();
+                snake.grow();
+            }
+
+        
+        snake.moveSnake();
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), {0,0});
     }
     return 0;
